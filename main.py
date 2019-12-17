843086649d4cd7baba2b6b677245adcd1a685001
Greg DeVore <greg.h.devore@gmail.com>
2019-12-17 07:24:46 -0800

    Adding python files

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donor, Donation
 
 app = Flask(__name__)
 
@@ -15,9 +15,19 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create', methods=['GET','POST'])
+def create():
+    if request.method == 'POST': # POST request, process donation
+        # Retrieve donor using name from form
+        donor = Donor.select().where(Donor.name == request.form['donor']).get()
+        # Add donation to database, credited to donor
+        Donation(value=request.form['donation'], donor=donor).save()
+        # Redirect to home page
+        return redirect(url_for('all'))
+    else: # GET request, display create template
+        return render_template('create.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import random
 
-from model import db, Donor, Donation 
+from model import db, Donor, Donation
 
 db.connect()
 
 # This line will allow you "upgrade" an existing database by
 # dropping all existing tables from it.
 db.drop_tables([Donor, Donation])
-
 db.create_tables([Donor, Donation])
 
 alice = Donor(name="Alice")
@@ -23,4 +22,3 @@ donors = [alice, bob, charlie]
 
 for x in range(30):
     Donation(donor=random.choice(donors), value=random.randint(100, 10000)).save()
-
